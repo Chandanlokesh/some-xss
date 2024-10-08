50bccec4bc825bfb5bee5f9c47b041e719c48546
Chandanlokesh <chandanlokesh17@gmail.com>
2024-10-08 09:22:35 +0530

    scanner

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const io = require('socket.io')(http);
 
 app.use(express.json());
 app.use(express.static('public')); // Serve static files from the public directory
+app.use('/assets', express.static('assets'));
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/your_database_name', {
